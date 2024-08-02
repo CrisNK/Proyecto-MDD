4b49aae704ab436e0f3f7f355d634d3ea0a36e8d
Bastian Arriagada Quero <bastian.arriagada2201@alumnos.ubiobio.cl>
2024-08-02 14:17:28 -0400

    Implementacion inscripcion a un evento

--- frontend/src/helpers/alert.js
+++ frontend/src/helpers/alert.js
@@ -6,7 +6,7 @@ export const showSuccessAlert = () => {
         icon: "success",
         title: "Inscripción enviada!",
         showConfirmButton: false,
-        timer: 1500,
+        timer: 3500,
       });
 }
 
@@ -16,6 +16,16 @@ export const showErrorAlert = () => {
         icon: "error",
         title: "Error al enviar la inscripción!",
         showConfirmButton: false,
-        timer: 1500,
+        timer: 3500,
       });
+}
+
+export const noEncontrado = () => {
+  Swal.fire({
+      position: "top-end",
+      icon: "error",
+      title: "Evento no encontrado!",
+      showConfirmButton: false,
+      timer: 3500,
+    });
 }
